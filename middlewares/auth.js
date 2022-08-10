cfd2f7a55bf30e69385bb4412dd7b92e8df4be7a
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-08-10 16:34:50 +0530

    fix: auth middleware and find by email

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,7 @@
 import admin from "../config/firebase";
+import Developer from "../models/developer";
+import DeveloperService from "../services/v1/DeveloperService";
+const developerService = new DeveloperService(Developer);
 
 export const isAuthorized = async (req, res, next) => {
   try {
@@ -7,12 +10,15 @@ export const isAuthorized = async (req, res, next) => {
     const decodedToken = await admin.auth().verifyIdToken(token);
 
     if (decodedToken) {
-      req.user = decodedToken;
+      const user = await developerService.findByEmailOrCreateIfNotFound(decodedToken);
+      console.log(user)
+      req.user = user;
       return next();
     }
 
     return res.status(401).json({ success: false, message: "Unauthorized" });
   } catch (err) {
+    console.log(err)
     return res.status(401).json({ success: false, message: "Unauthorized" });
   }
 };
