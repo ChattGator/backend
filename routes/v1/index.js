cd38545b2d1129b576333ac36d76369a1522f45d
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-08-10 17:11:51 +0530

    feat: user CRUD API

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { isAuthorized } from "../../middlewares/auth";
 import developer from "./developer";
 import project from "./project";
+import user from "./user";
 import test from "./test";
 
 const router = express.Router();
@@ -9,6 +10,7 @@ const router = express.Router();
 router.use("/test", test);
 router.use("/developer", developer);
 router.use("/project", isAuthorized, project);
+router.use("/user", user);
 router.use("/", isAuthorized, () => console.log("helloworld v1 api"));
 
 export default router;
