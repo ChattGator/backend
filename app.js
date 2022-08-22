07cf717df39de74e945d2bfae3d4eeb3fcf2152b
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-08-22 10:17:26 +0530

    feat: socket messaging

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+import http from "http";
 import express from "express";
 import * as dotenv from "dotenv";
 import morgan from "morgan";
 import apiRoutes from "./routes/";
+import { Server } from "socket.io";
+import cors from "cors";
+import MessageService from "./services/v1/MessageService";
+import message from "./models/message";
+const messageService = new MessageService(message);
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 dotenv.config();
 app.use(express.json());
@@ -13,4 +30,30 @@ app.use(morgan("tiny"));
 // router middleware
 app.use("/api", apiRoutes);
 
-export default app;
+// socket
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ user, groupId }) => {
+    socket.join(groupId);
+
+    socket.emit("message", {
+      user: { name: "Server" },
+      message: `Welcome ${user.name}`,
+    });
+  });
+
+  socket.on("chatMessage", ({ groupId, user, message }) => {
+    try {
+      messageService.insert({
+        userId: user.id,
+        groupId: groupId,
+        message,
+        messageType: "Text",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    io.to(groupId).emit("message", { user, message });
+  });
+});
+
+export default server;
